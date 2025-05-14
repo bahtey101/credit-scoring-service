d43057cb8bf28470a1993c30b0567897db3a4ca0
Dmitry Bakhaev <dbakhaev@workmovr.com>
2025-05-14 09:51:52 +0400

    add CORS policy middleware

--- internal/http/handlers/handler.go
+++ internal/http/handlers/handler.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	"github.com/bahtey101/credit-scoring-service/internal/app/scoringservice"
@@ -26,6 +29,15 @@ func (h *Hanlder) InitRoutes() *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:4200"},
+		AllowMethods:     []string{"POST", "GET", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	api := router.Group(("/api"))
 	{
